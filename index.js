ce3a43db1bc6f08d582a3726450c534cdfcd42b3
Xavier Collantes <xcollantes@zagmail.gonzaga.edu>
2017-09-16 00:27:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a, b)
+{
+  return a + b;
+}
+
+function substract(a, b)
+{
+  return a - b;
+}
+function multiply(a, b)
+{
+  return a * b;
+}
+
+function divide(a, b)
+{
+  return a / b;
+}
+
+function inc(a)
+{
+  return a++;
+}
+
+function dec(a)
+{
+  return a--;
+}
+
+function makeInt(a)
+{
+  return a.parseInt(a, 10);
+}
+
+function preserveDecimal(a)
+{
+  return parseFloat(a);
+}
